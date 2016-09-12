01024379502e6b11ef6c2c60e9e79c17e7d51170
Ruijie Zhang <ruijieapplication@gmail.com>
2016-09-11 21:00:20 -0400

    ng-switch is working

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,7 +16,12 @@ angular.module('OPC.controllers', []).
   controller('app', function ($scope) {
     // write Ctrl here
     console.log('controller apps launching');
+
+    // hide the oraganic title 
     $scope.indexContent.display = false;
+
+    // set the ng-switch view to default
+    $scope.view ='default';
 
   }).
   controller('MyCtrl2', function ($scope) {
